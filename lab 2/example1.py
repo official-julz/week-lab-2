ddac45c0696f6798de269860b2a82abf034dfb71
official-julz <juliusstrickland47@gmail.com>
2024-08-16 11:56:17 +1200

    Add files via upload

---
+++ lab 2/example1.py
@@ -0,0 +1,14 @@
+# square.py
+# 
+# author: A. N. Other
+# date: September 2016
+
+side_j = int(input("Please enter the length of one side of the square\n\n"))
+print("\n The area of your square is ", side_j**2, "\n\n")
+
+# Testing
+...
+print("my assertiond are:"
+      "\nj = 5 output = 25"
+      "\nj = 12 output = 144")
+...
